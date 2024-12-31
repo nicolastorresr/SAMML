974617bd9890cea5ce39990d14f1dcd3e083b951
Nicolás Torres <nicolas.torresr@alumnos.usm.cl>
2024-12-31 09:21:29 -0300

    Create run_simulation.py

---
+++ run_simulation.py
@@ -0,0 +1,14 @@
+"""
+Script: run_simulation.py
+
+Description:
+This script serves as the main entry point for running simulations. It allows users to 
+configure parameters, initialize the simulation environment, and save results for analysis.
+
+Dependencies:
+- simulation: For configuring and executing the simulation.
+- utils: For saving and visualizing simulation results.
+"""
+
+from modules.simulation import Simulation
+from modules.utils import log_metrics
